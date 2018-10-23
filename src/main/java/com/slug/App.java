6448a5309e0aa1337c408324e94ed92293a5a68b
fengjing.yfj <fengjing.yfj@alibaba-inc.com>
2018-10-23 19:32:17 +0800

    first commit

---
+++ src/main/java/com/slug/App.java
@@ -0,0 +1,13 @@
+package com.slug;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
